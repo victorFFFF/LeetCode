7a814f5483bfde1ed5ef905ae215f7b908469582
Victor Li <31644062+victorFFFF@users.noreply.github.com>
2019-12-18 16:13:41 -0500

    Create validAnagram.java

---
+++ validAnagram.java
@@ -0,0 +1,24 @@
+/*Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false */
+
+class validAnagram {
+        
+  public boolean isAnagram(String s, String t) {
+    if (s.length() != t.length()) {
+        return false;
+    }
+    char[] str1 = s.toCharArray();
+    char[] str2 = t.toCharArray();
+    Arrays.sort(str1);
+    Arrays.sort(str2);
+    return Arrays.equals(str1, str2);
+}
+}
