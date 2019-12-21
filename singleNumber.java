57faf4f18bc3fb01090b38d71479e4876076bf8f
Victor Li <31644062+victorFFFF@users.noreply.github.com>
2019-12-20 19:56:33 -0500

    Create singleNumber.java

---
+++ singleNumber.java
@@ -0,0 +1,38 @@
+/*Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4*/
+
+class singleNumber {
+    public int singleNumber(int[] nums) {
+        
+        HashMap<Integer, Integer> hMap = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            hMap.put(nums[i],0);
+        }
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            hMap.put(nums[i],hMap.get(nums[i])+1);
+        }
+        
+        for(int i = 0; i < nums.length; i++)
+        {
+            if( hMap.get(nums[i]) == 1)
+                return nums[i];
+        }
+        return -1;
+    }
+}
