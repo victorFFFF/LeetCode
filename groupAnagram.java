bbd1785b08f6c1d626ae834c0c678ef683879fe7
Victor Li <31644062+victorFFFF@users.noreply.github.com>
2019-12-27 23:26:03 -0500

    Create groupAnagram.java

---
+++ groupAnagram.java
@@ -0,0 +1,48 @@
+class groupAnagram {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+    String[] associateArray = strs.clone();
+
+      //Sorting each individual element in alphabetical order
+      for(int i = 0; i < strs.length; i++)
+      {
+        char[] arrayChar = strs[i].toCharArray();
+        Arrays.sort(arrayChar);
+        strs[i] = new String(arrayChar);
+      }
+
+      List< List<String> > wholeList = new ArrayList<>();
+      
+    //Loop through entire sorted array and compare each element, if the 2 strings are equal then insert it into a arrayList named partList.
+      for(int i = 0; i < strs.length; i++)
+      { 
+          //make a new arraylist for partLis everytime i increase by 1
+          List<String> partList = new ArrayList<>();
+          
+        if(strs[i] != null)
+        {
+          partList.add(associateArray[i]);
+
+          if( i != strs.length-1)
+            {
+                for(int j = i ; j < strs.length-1; j++)
+                {
+                            if(strs[i].equals(strs[j+1]))
+                            {
+                              partList.add(associateArray[j+1]);
+                              strs[j+1] = null;
+                            }
+                }
+            }
+         //Setting strs[i] to null so we will know if it is already inserted into the partList
+          strs[i] = null;  
+            
+        //Add the partList into a elemnt of wholeList.
+        //Each element wholeList contains a arrayList of partList
+          wholeList.add(partList);
+        }
+      }
+      return wholeList;      
+  }
+    
+}
