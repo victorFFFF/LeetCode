cf034e9dc13f1e734b8b5da6fd2b9843b291a3bf
Victor Li <31644062+victorFFFF@users.noreply.github.com>
2020-01-04 17:08:57 -0500

    Update increasingOrderSearchTree.java

--- increasingOrderSearchTree.java
+++ increasingOrderSearchTree.java
@@ -45,7 +45,8 @@ Each node will have a unique integer value from 0 to 1000.*/
  *     TreeNode right;
  *     TreeNode(int x) { val = x; }
  * }
- */
+ */    
+//RECURSION VERSION
 class increasingOrderSearchTree {
     
     LinkedList<Integer> list = new LinkedList<>();
@@ -86,3 +87,71 @@ class increasingOrderSearchTree {
     }
 
 }
+
+
+//ITErATIVE VERSION
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    
+    public TreeNode increasingBST(TreeNode root) 
+    {
+      if(root != null)
+      {
+          
+        TreeNode pointer = null;
+        inOrderTraversal(root);
+
+        LinkedList<Integer>list = inOrderTraversal(root);
+          
+        root = null;
+        root = new TreeNode(list.getFirst());
+        list.removeFirst();
+        pointer = root;
+          
+        while( !list.isEmpty() )
+        {
+            pointer.right = new TreeNode(list.getFirst());
+            list.removeFirst();
+            pointer = pointer.right;
+        }
+          return root;
+      }
+             
+    return null;
+        
+     }
+    
+    public LinkedList inOrderTraversal(TreeNode root)
+    {
+
+        Stack<TreeNode> orderStack = new Stack <>();
+        LinkedList<Integer> theList = new LinkedList<>();
+        TreeNode pointer = root;
+        
+        while( pointer != null || orderStack.size() != 0)
+        {
+            while(pointer != null)
+            {
+              orderStack.push(pointer);
+              pointer = pointer.left;
+            }
+            
+            pointer = orderStack.pop();
+            theList.add(pointer.val);
+            
+            pointer = pointer.right;
+        }
+        
+        return theList;
+    }
+    
+}
+        
