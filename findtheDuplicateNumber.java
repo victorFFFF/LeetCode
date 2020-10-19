eb32de6834a44044334c9842950f1ae8f13c4d37
Victor Li <victorisnice@Victors-MacBook-Pro-2.local>
2020-10-19 15:47:12 -0400

    duplicateNUM

---
+++ findtheDuplicateNumber.java
@@ -0,0 +1,46 @@
+/*
+Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+There is only one duplicate number in nums, return this duplicate number.
+
+Follow-ups:
+
+How can we prove that at least one duplicate number must exist in nums? 
+Can you solve the problem without modifying the array nums?
+Can you solve the problem using only constant, O(1) extra space?
+Can you solve the problem with runtime complexity less than O(n2)?
+ 
+
+Example 1:
+
+Input: nums = [1,3,4,2,2]
+Output: 2
+Example 2:
+
+Input: nums = [3,1,3,4,2]
+Output: 3
+Example 3:
+
+Input: nums = [1,1]
+Output: 1
+Example 4:
+
+Input: nums = [1,1,2]
+Output: 1
+*/
+
+class findtheDuplicateNumber {
+    public int findDuplicate(int[] nums) {
+        
+        int [] array = new int [30000];
+        for( int i = 0; i < nums.length; i++)
+        {
+            if( array[ nums[i] ] == 0)
+                array[ nums[i] ] = nums[i];
+            else
+                return nums[i];
+        
+            }
+        return -1;
+    }
+}
