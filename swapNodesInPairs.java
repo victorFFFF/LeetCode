1c4bace15b0e696698e38375566a2f942f394b48
Victor Li <victor.li.1995@hotmail.com>
2021-03-25 10:14:21 -0400

    updated swapPairNodes

--- swapNodesInPairs.java
+++ swapNodesInPairs.java
@@ -61,20 +61,3 @@ class swapNodesInPairs {
     }
 
 }
-
-    // Simple solution but not my solution
-
-public ListNode swapNodesInPairs(ListNode head) {
-    ListNode dummy = new ListNode(0);
-    dummy.next = head;
-    ListNode current = dummy;
-    while (current.next != null && current.next.next != null) {
-        ListNode first = current.next;
-        ListNode second = current.next.next;
-        first.next = second.next;
-        current.next = second;
-        current.next.next = first;
-        current = current.next.next;
-    }
-    return dummy.next;
-}
